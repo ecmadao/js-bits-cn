0e7bae589c9bd2d79b02bb63dc4b0603c3741bb7
ecmadao <wlec@outlook.com>
2016-11-02 09:46:13 +0800

    transition “Array every() some()”

--- README.md
+++ README.md
@@ -8,14 +8,15 @@
 
 ## Menu
 
-- [AMD CommonJS and ES6 模块机制的使用](js/amd-commonjs-es6modules.js)
+- [AMD、CommonJS 和 ES6 模块机制的使用](js/amd-commonjs-es6modules.js)
 - [Array concat() 和 push() 方法](js/array-concat-push.js)
+- [Array every() 和 some() 方法](js/array-every-some.js)
 
 ## Todo
 
 - [x] [AMD CommonJS and ES6 Modules Usage](js/amd-commonjs-es6modules.js)
 - [x] [Array concat() push()](js/array-concat-push.js)
-- [ ] [Array every() some()](js/array-every-some.js)
+- [x] [Array every() some()](js/array-every-some.js)
 - [ ] [Array filter() map() reduce()](js/array-filter-map-reduce.js)
 - [ ] [Array forEach()](js/array-foreach.js)
 - [ ] [Array pass by val vs reference](js/array-pass-by-val-reference.js)
--- js/array-every-some.js
+++ js/array-every-some.js
@@ -1,24 +1,22 @@
 /**
- * Array every() and some() (instead of forEach)
+ * Array every() and some() (而不是使用 forEach)
  *
- * Another limitation with forEach is that you can’t break out of the loop (and no, using exceptions doesn’t count).
- * As a result, I’ve seen developers either revert back to for loops when needing to be able to break out,
- * or needlessly iterate over extraneous array elements.
+ * 有时候，我们可能需要对一个 array 进行循环判断，查看其是否满足一些条件（或者其他的需求）。
+ * forEach 方法的一个限制是，使用中无法跳出循环，结果，有时候会看见有开发者退而使用了 for 循环，或者在迭代时使用不必要的 array 元素。
  *
- * A better solution exists in the form of the lesser known every() and some() array iteration methods.
- * every iterates until the provided callback returns false, and some iterates until the provided callback returns true.
+ * 一个更好的选择是使用较为小众的 every() 和 some() 方法进行遍历，
+ * 每一次的遍历，都会将元素代入回调，最终返回 true 或者 false
  *
- * Both every and some have the same browser support as forEach.
+ * 浏览器对 every 和 some 的支持与 forEach 的一样。
  *
- * @Reference:
+ * @参考资料:
  * http://engineering.wix.com/2015/04/21/javascript-the-extra-good-parts/
  * https://coderwall.com/p/_ggh2w/the-array-native-every-filter-map-some-foreach-methods
  *
  */
 
-// some() breaks once it returns true
+// 只要有任意一次遍历返回 true，some() 就会中断，并返回 true；否则返回 false
 (function () {
-  // God of cricket
   var ar = ['Lara', 'Sachin', 'De Villiers'];
   ar.some(function (v) {
     if (v === 'Sachin') {
@@ -28,9 +26,8 @@
   });
 })();
 
-// every() breaks once it returns false
+// 只要有任意一次遍历返回 false，every() 就会中断，并返回 false；否则返回 true
 (function () {
-  // Music Composers
   var ar = ['Hans Zimmer', 'Bill Clinton', 'Clint Mansell'];
   ar.every(function (v) {
     if (v === 'Bill Clinton') {
@@ -40,7 +37,7 @@
   });
 })();
 
-// every() and some() in an example
+// every() 和 some() 实例
 (function () {
   function isBigEnough(element) {
     return element >= 10;
